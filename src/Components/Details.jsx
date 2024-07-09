5c046b866887f021b0de11cf51766b3eed89defb
Raj <rajsunmukhani123@gmail.com>
2024-07-09 23:26:49 +0530

    updated routing

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from '../utils/axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Loading from './Loading';
 
 const Details = () => {
-  return (
+  const {id} = useParams();
+  
+  const [tempProduct,setTempProduct] = useState(null);
+
+  const getProduct = async() => {
+    const data = await axios.get(`/products/${id}`)
+    setTempProduct(data);
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+  
+
+  return tempProduct ? (
     <div className='w-full h-screen'>
       <div className='w-[75%] h-screen flex items-center m-auto gap-48'>
-        <div className='h-content flex justify-center gap-36'>
-          <img className='w-[30%] h-[60%] object-cover' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
-          <div className='content flex flex-col gap-5 w-[40%] py-5'>
-            <h1 className='text-4xl'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-            <h3 className='text-zinc-400 font-semibold text-2xl'>Category : Men's Clothing</h3>
-            <h2 className='text-6xl font-semibold'>$ 109.95</h2>
-            <p className='text-xl text-zinc-700'>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+        <div className='h-content flex justify-center align-center gap-36'>
+          <img className='w-[40%] h-[60%] object-cover' src={tempProduct.data.image} alt="" />
+          <div className='content flex flex-col gap-5 w-[60%] py-5'>
+            <h1 className='text-4xl'>{tempProduct.data.title}</h1>
+            <h3 className='text-zinc-400 font-semibold text-2xl'>Category : {tempProduct.data.category}</h3>
+            <h2 className='text-6xl font-semibold'>$ {tempProduct.data.price}</h2>
+            <p className='text-l w-[100%] h-[20%] text-ellipsis whitespace-wrap overflow-hidden text-zinc-700'>{tempProduct.data.description}</p>
             <div className="buttons flex gap-10 w-full py-8 flex">
               <Link className='border border-blue-200 text-blue-500 text-center w-24 py-2'>Edit</Link>
               <Link className='border border-red-200 text-red-500 text-center w-24 py-2'>Delete</Link>
@@ -20,7 +36,7 @@ const Details = () => {
         </div>
       </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Details
